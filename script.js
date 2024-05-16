8bad84d452ca971369e154ba3a148e5f87f8390c
YY T <changiyy07@gmail.com>
2024-05-16 11:40:35 +0800

    Check if user make valid action (can move) before new tile populated

--- script.js
+++ script.js
@@ -24,15 +24,31 @@ function setupInput() {
 async function handleInput(e) {
   switch (e.key) {
     case "ArrowUp":
+      if (!canMoveUp()) {
+        setupInput()
+        return
+      }
       await moveUp()
       break
     case "ArrowDown":
+      if (!canMoveDown()) {
+        setupInput()
+        return
+      }
       await moveDown()
       break
     case "ArrowLeft":
+      if (!canMoveLeft()) {
+        setupInput()
+        return
+      }
       await moveLeft()
       break
     case "ArrowRight":
+      if (!canMoveRight()) {
+        setupInput()
+        return
+      }
       await moveRight()
       break
     default:
@@ -108,3 +124,30 @@ function slideTiles(cells) {
 }
 
 // Note: canAccept is a Cell class method
+
+function canMoveUp() {
+  return canMove(grid.cellsByColumn)
+}
+
+function canMoveDown() {
+  return canMove(grid.cellsByColumn.map(column => [...column].reverse()))
+}
+
+function canMoveLeft() {
+  return canMove(grid.cellsByRow)
+}
+
+function canMoveRight() {
+  return canMove(grid.cellsByRow.map(row => [...row].reverse()))
+}
+
+function canMove(cells) {
+  return cells.some(group => {
+    return group.some((cell, index) => {
+      if (index === 0) return false
+      if (cell.tile == null) return false
+      const moveToCell = group[index - 1]
+      return moveToCell.canAccept(cell.tile)
+    })
+  })
+}
